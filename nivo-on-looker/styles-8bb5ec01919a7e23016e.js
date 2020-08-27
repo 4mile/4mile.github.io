b1038c15dbb53808b259d80fd91eae3e20b50e87
David Stocker <david@4mile.io>
2020-08-27 13:05:10 -0700

    removing nivo

--- nivo-on-looker/styles-8bb5ec01919a7e23016e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[73],{167:function(n,w,o){}}]);
-//# sourceMappingURL=styles-8bb5ec01919a7e23016e.js.map
--- nivo-on-looker/styles-967082b1165e8448cc38.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[74],[]]);
-//# sourceMappingURL=styles-967082b1165e8448cc38.js.map
